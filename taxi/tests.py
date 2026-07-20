510de01c7c93abbd57aea20f74f3f470926adce8
MykolaLozanDevelopment <mykola.lozan.development@gmail.com>
2026-07-20 19:50:42 +0300

    Solution

--- taxi/tests.py
+++ taxi/tests.py
@@ -1,3 +1,71 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
+from django.urls import reverse
 
-# Create your tests here.
+from taxi.models import Manufacturer, Car
+
+
+class PublicViewsTest(TestCase):
+    def test_login_required_for_manufacturer_list(self):
+        response = self.client.get(reverse("taxi:manufacturer-list"))
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_login_required_for_car_list(self):
+        response = self.client.get(reverse("taxi:car-list"))
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_login_required_for_driver_list(self):
+        response = self.client.get(reverse("taxi:driver-list"))
+        self.assertNotEqual(response.status_code, 200)
+
+
+class SearchTests(TestCase):
+    def setUp(self):
+        self.user = get_user_model().objects.create_user(
+            username="test_user",
+            password="testpass123",
+            license_number="ABC12345",
+        )
+        self.client.force_login(self.user)
+
+        self.manufacturer = Manufacturer.objects.create(
+            name="Toyota", country="Japan"
+        )
+        Manufacturer.objects.create(name="Ford", country="USA")
+
+        self.car = Car.objects.create(
+            model="Camry", manufacturer=self.manufacturer
+        )
+        Car.objects.create(model="Focus", manufacturer=self.manufacturer)
+
+        get_user_model().objects.create_user(
+            username="another_driver",
+            password="testpass123",
+            license_number="XYZ98765",
+        )
+
+    def test_manufacturer_search_by_name(self):
+        response = self.client.get(
+            reverse("taxi:manufacturer-list"), {"name": "Toyota"}
+        )
+        self.assertContains(response, "Toyota")
+        self.assertNotContains(response, "Ford")
+
+    def test_car_search_by_model(self):
+        response = self.client.get(
+            reverse("taxi:car-list"), {"model": "Camry"}
+        )
+        self.assertContains(response, "Camry")
+        self.assertNotContains(response, "Focus")
+
+    def test_driver_search_by_username(self):
+        response = self.client.get(
+            reverse("taxi:driver-list"), {"username": "test_user"}
+        )
+        self.assertContains(response, "test_user")
+        self.assertNotContains(response, "another_driver")
+
+    def test_empty_search_returns_all(self):
+        response = self.client.get(reverse("taxi:car-list"))
+        self.assertContains(response, "Camry")
+        self.assertContains(response, "Focus")
--- taxi/views.py
+++ taxi/views.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
 from django.http import HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse_lazy
@@ -6,7 +7,14 @@ from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .models import Driver, Car, Manufacturer
-from .forms import DriverCreationForm, DriverLicenseUpdateForm, CarForm
+from .forms import (
+    DriverCreationForm,
+    DriverLicenseUpdateForm,
+    CarForm,
+    DriverSearchForm,
+    CarSearchForm,
+    ManufacturerSearchForm,
+)
 
 
 @login_required
@@ -36,6 +44,23 @@ class ManufacturerListView(LoginRequiredMixin, generic.ListView):
     template_name = "taxi/manufacturer_list.html"
     paginate_by = 5
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        name = self.request.GET.get("name", "")
+        context["search_form"] = ManufacturerSearchForm(
+            initial={"name": name}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = Manufacturer.objects.all()
+        form = ManufacturerSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                name__icontains=form.cleaned_data["name"]
+            )
+        return queryset
+
 
 class ManufacturerCreateView(LoginRequiredMixin, generic.CreateView):
     model = Manufacturer
@@ -57,7 +82,23 @@ class ManufacturerDeleteView(LoginRequiredMixin, generic.DeleteView):
 class CarListView(LoginRequiredMixin, generic.ListView):
     model = Car
     paginate_by = 5
-    queryset = Car.objects.select_related("manufacturer")
+    queryset = Car.objects.all().select_related("manufacturer")
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        model = self.request.GET.get("model", "")
+        context["search_form"] = CarSearchForm(initial={"model": model})
+        return context
+
+    def get_queryset(self):
+        queryset = Car.objects.all().select_related("manufacturer")
+        form = CarSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                model__icontains=form.cleaned_data["model"]
+            )
+        return queryset
+
 
 
 class CarDetailView(LoginRequiredMixin, generic.DetailView):
@@ -82,8 +123,25 @@ class CarDeleteView(LoginRequiredMixin, generic.DeleteView):
 
 
 class DriverListView(LoginRequiredMixin, generic.ListView):
-    model = Driver
+    model = get_user_model()
     paginate_by = 5
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        username = self.request.GET.get("username", "")
+        context["search_form"] = DriverSearchForm(
+            initial={"username": username}
+        )
+        return context
+
+    def get_queryset(self):
+        queryset = get_user_model().objects.all()
+        form = DriverSearchForm(self.request.GET)
+        if form.is_valid():
+            return queryset.filter(
+                username__icontains=form.cleaned_data["username"]
+            )
+        return queryset
 
 
 class DriverDetailView(LoginRequiredMixin, generic.DetailView):
@@ -104,7 +162,7 @@ class DriverLicenseUpdateView(LoginRequiredMixin, generic.UpdateView):
 
 class DriverDeleteView(LoginRequiredMixin, generic.DeleteView):
     model = Driver
-    success_url = reverse_lazy("")
+    success_url = reverse_lazy("taxi:driver-list")
 
 
 @login_required
